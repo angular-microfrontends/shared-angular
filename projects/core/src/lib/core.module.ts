c9183f6ee5e415f9cdbd4266935bd9cf8a53cebd
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-01-30 08:21:35 -0300

    Shared service

--- projects/core/src/lib/core.module.ts
+++ projects/core/src/lib/core.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CoreComponent } from './core.component';
+import { CoreService } from './core.service';
 
 @NgModule({
   declarations: [CoreComponent],
   imports: [
   ],
   exports: [CoreComponent],
+  providers: [
+    CoreService,
+  ],
 })
 export class CoreModule { }
--- projects/core/src/lib/core.service.ts
+++ projects/core/src/lib/core.service.ts
@@ -3,4 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class CoreService { }
+export class CoreService {
+  private message = 'Hello from core library';
+
+  readMessage(): string {
+    return this.message;
+  }
+}
